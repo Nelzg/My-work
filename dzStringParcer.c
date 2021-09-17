b85237d830b6187d11c68bc4f90bedf023edd1a6
Nelzg <90206387+Nelzg@users.noreply.github.com>
2021-09-17 23:46:07 +0300

    Update dzStringParcer.c

--- dzStringParcer.c
+++ dzStringParcer.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-int StringSplit(char *string, char *delimiters, char **token, int *tokensCount) {
+void StringSplit(char *string, char *delimiters, char **token, int *tokensCount) {
 	int i = 0;
 	char *saveToken = string;
 	int size = 3;
@@ -15,47 +15,4 @@ int StringSplit(char *string, char *delimiters, char **token, int *tokensCount) 
 		tokensCount[i] = strlen(token[i]);
 		i++;
 	}
-	printf("\n");
-	return i;
-}
-
-int main() {
-	char *string;
-	char *delimiters;
-	char **token;
-	int *tokensCount;
-	int i,n,size;
-	size = 3;	
-
-	string = (char *) malloc (size * sizeof(char));
-	token = (char **) malloc (size * sizeof(char *));
-	tokensCount = (int *) malloc (size * sizeof(int));
-	delimiters = (char *) malloc (size * sizeof(char));
-
-	i = 0;
-	while ((string[i] = getchar()) != '\n' ) {
-		if (i == (size - 1)) {
-			size = size + 3;
-			string = (char *) realloc (string, size * sizeof(char));
-		}
-		i++;
-	}
-	string[i] = '\0';
-
-	i = 0;
-	while ((delimiters[i] = getchar()) != '\n' ) {
-                if (i == (size - 1)) {
-			size = size + 3;
-                        delimiters = (char *) realloc (delimiters, size * sizeof(char));
-                }
-                i++;
-        }
-	delimiters[i] = '\0';
-
-	n = StringSplit(string, delimiters, token, tokensCount);
-	for (i = 0; i < n; i++) {
-		printf("%s , %d\n", token[i], tokensCount[i]);
-	}
-
-	return 0;
 }
